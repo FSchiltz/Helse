7728b4b31aac858fd007ea9c31849dc4a6de6c44
FSchiltz <1764553+FSchiltz@users.noreply.github.com>
2023-07-31 15:45:22 +0200

    Add color for each event description

--- App/lib/ui/blocs/events/events_graph.dart
+++ App/lib/ui/blocs/events/events_graph.dart
@@ -8,11 +8,7 @@ class EventGraph extends StatelessWidget {
   final List<Event> events;
   final DateTimeRange date;
 
-  late int viewRange;
-
-  EventGraph(this.events, this.date, {super.key}) {
-    viewRange = _secondsBetween(date.start, date.end);
-  }
+  const EventGraph(this.events, this.date, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +29,7 @@ class EventGraph extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         children: <Widget>[
           Stack(fit: StackFit.loose, children: <Widget>[
+            buildGrid(chartViewWidth),
             Container(
                 margin: const EdgeInsets.only(top: 25.0),
                 child: Column(
@@ -55,6 +52,9 @@ class EventGraph extends StatelessWidget {
 
   List<Widget> buildChartBars(List<Event> data, double chartViewWidth) {
     List<Widget> chartBars = [];
+    Map<String, Color> colors = {};
+
+    var viewRange = _secondsBetween(date.start, date.end);
     var chartFactor = chartViewWidth / viewRange;
 
     for (var n in data) {
@@ -63,7 +63,7 @@ class EventGraph extends StatelessWidget {
       var end = n.stop ?? date.end;
 
       var remainingWidth = _distanceInSeconds(start, end);
-      var color = _stateColor();
+      var color = _stateColor(colors, n.description);
       if (remainingWidth > 0) {
         chartBars.add(Container(
           decoration: BoxDecoration(color: color.withAlpha(100), borderRadius: BorderRadius.circular(10.0)),
@@ -87,9 +87,34 @@ class EventGraph extends StatelessWidget {
     return chartBars;
   }
 
-  Color _stateColor() {
-    var r = Random();
-    return Color.fromRGBO(r.nextInt(256), r.nextInt(256), r.nextInt(256), 0.75);
+  Widget buildGrid(double chartViewWidth) {
+    List<Widget> gridColumns = [];
+
+    var viewRange = _secondsBetween(date.start, date.end) / (60 * 60);
+
+    for (int i = 0; i <= viewRange; i++) {
+      gridColumns.add(Container(
+        decoration: BoxDecoration(border: Border(right: BorderSide(color: Colors.white.withAlpha(75), width: 0.5))),
+        width: chartViewWidth / viewRange,
+      ));
+    }
+
+    return Row(
+      children: gridColumns,
+    );
+  }
+
+  Color _stateColor(Map<String, Color> colors, String? state) {
+    if (state == null) return Colors.white;
+
+    if (colors.containsKey(state)) {
+      return colors[state]!;
+    } else {
+      var r = Random();
+      var color = Color.fromRGBO(r.nextInt(256), r.nextInt(256), r.nextInt(256), 0.75);
+      colors[state] = color;
+      return color;
+    }
   }
 
   int _secondsBetween(DateTime from, DateTime to) {
@@ -105,7 +130,8 @@ class EventGraph extends StatelessWidget {
   }
 
   int _distanceInSeconds(DateTime start, DateTime end) {
-    int seconds = _secondsBetween(start, end);
+    var seconds = _secondsBetween(start, end);
+    var viewRange = _secondsBetween(date.start, date.end);
 
     if (start.compareTo(date.start) >= 0 && start.compareTo(date.end) <= 0) {
       // The date is between the bond
