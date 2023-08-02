ba1b32fc961f3c67e590dc63a10ba487aedc5ad8
FSchiltz <1764553+FSchiltz@users.noreply.github.com>
2023-08-02 22:14:18 +0200

    remove unused code

--- App/lib/logic/event.dart
+++ App/lib/logic/event.dart
@@ -8,29 +8,3 @@ sealed class ChangedEvent<T> {
   final String field;
   final T? value;
 }
-
-final class FileChangedEvent extends ChangedEvent<XFile> {
-  const FileChangedEvent(XFile value, String field) : super(value, field);
-}
-
-final class TextChangedEvent extends ChangedEvent<String> {
-  const TextChangedEvent(String value, String field) : super(value, field);
-}
-
-final class IntChangedEvent extends ChangedEvent<int> {
-  const IntChangedEvent(int value, String field) : super(value, field);
-}
-
-final class BoolChangedEvent extends ChangedEvent<bool> {
-  const BoolChangedEvent(bool value, String field) : super(value, field);
-}
-
-final class DateChangedEvent extends ChangedEvent<DateTime> {
-  const DateChangedEvent(DateTime value, String field) : super(value, field);
-}
-
-final class SubmittedEvent extends ChangedEvent {
-  final void Function()? callback;
-
-  const SubmittedEvent(String field, {this.callback}) : super(null, field);
-}
