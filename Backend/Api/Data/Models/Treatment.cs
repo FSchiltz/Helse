2048359055f926547ca8d8c5f01716bd0c1937f2
FSchiltz <1764553+FSchiltz@users.noreply.github.com>
2023-08-09 20:47:02 +0200

    Database improvement

--- Backend/Api/Data/Models/Treatment.cs
+++ Backend/Api/Data/Models/Treatment.cs
@@ -8,12 +8,18 @@ public class Treatment
     [PrimaryKey]
     public long Id { get; set; }
 
-    [Column, NotNull]
-    public long PrescriptionId { get; set; }
+    [Column]
+    public long? PrescriptionId { get; set; }
 
     [Association(ThisKey = nameof(PrescriptionId), OtherKey = nameof(Data.Models.Prescription.Id))]
     public Prescription? Prescription { get; set; }
 
     [Column, NotNull]
     public int Type { get; set; }
+
+    [Column, NotNull]
+    public long PersonId { get; set; }
+
+    [Association(ThisKey = nameof(PersonId), OtherKey = nameof(Data.Models.Person.Id))]
+    public Person? Person { get; set; }
 }
