97dec9d38b11853008d77f7cab1974657fe6522e
FSchiltz <1764553+FSchiltz@users.noreply.github.com>
2023-08-24 17:46:27 +0200

    improve the import of data

--- Backend/Api/Data/Scripts/004-HealthData.sql
+++ Backend/Api/Data/Scripts/004-HealthData.sql
@@ -1,13 +1,13 @@
-INSERT INTO health.metrictype(description, name, unit)
-	VALUES (null, 'Heart', 'bpm'),
-    (null , 'Oxygen', '%'),
-    (null , 'Wheight', 'Kg'),
-    (null , 'Height', 'm'),
-    (null , 'Temperature', 'C'),    
-    (null , 'Steps', ''),
-    (null , 'Calories', 'kcal'), 
-    (null , 'Distance', 'm')    ;
+INSERT INTO health.metrictype(id, description, name, unit)
+	VALUES (1, null, 'Heart', 'bpm'),
+    (2, null , 'Oxygen', '%'),
+    (3,null , 'Wheight', 'Kg'),
+    (4,null , 'Height', 'm'),
+    (5,null , 'Temperature', 'C'),    
+    (6,null , 'Steps', ''),
+    (7,null , 'Calories', 'kcal'), 
+    (8,null , 'Distance', 'm')    ;
 
-    INSERT INTO health.EventType(description, name, standalone)
-	VALUES (null, 'Sleep', true),    
+    INSERT INTO health.EventType(id,description, name, standalone)
+	VALUES (1,null, 'Sleep', true),    
 	(null, 'Care', false)    ; 
