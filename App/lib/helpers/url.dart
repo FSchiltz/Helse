bbaf723d3825acad87bd426e51f893f4aa90dedf
FSchiltz <1764553+FSchiltz@users.noreply.github.com>
2024-04-30 08:53:51 +0200

    Clean up warnings

--- App/lib/helpers/url.dart
+++ App/lib/helpers/url.dart
@@ -2,6 +2,9 @@ import 'dart:html' as html;
 
 class UrlHelper {
   static void removeParam() {
-    html.window.history.replaceState(null, 'title', '/');
+    dynamic state = {};
+    state.serialCount = 0;
+    state.state = null;
+    html.window.history.replaceState(state, 'title', '/');
   }
 }
