314c0bcd458a3aba83fe0cc2a3b8c3989cd58b12
FSchiltz <1764553+FSchiltz@users.noreply.github.com>
2024-05-04 13:16:15 +0200

    Update android runtime

---
+++ App/android/app/src/main/kotlin/com/example/helse/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.helse
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- App/android/app/src/main/kotlin/com/helse/app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.helse.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
