1a4b1d9d509be7a484704e21e66dd4716a276279
Francois Schiltz <1764553+FSchiltz@users.noreply.github.com>
2024-05-07 16:04:53 +0200

    Fix flickering of the settings (#96)

--- App/lib/ui/blocs/administration/settings/oauth.dart
+++ App/lib/ui/blocs/administration/settings/oauth.dart
@@ -3,6 +3,7 @@ import 'package:helse/logic/d_i.dart';
 import 'package:helse/ui/theme/notification.dart';
 
 import '../../../../services/swagger/generated_code/swagger.swagger.dart';
+import '../../../theme/custom_switch.dart';
 import '../../../theme/square_text_field.dart';
 import '../../../theme/loader.dart';
 
@@ -15,15 +16,6 @@ class OauthView extends StatefulWidget {
 
 class _OauthViewState extends State<OauthView> {
   Oauth? _settings;
-  final GlobalKey<FormState> _formKey = GlobalKey();
-
-  final TextEditingController _controllerId = TextEditingController();
-  final TextEditingController _controllerSecret = TextEditingController();
-  final TextEditingController _controllerAuth = TextEditingController();
-  final TextEditingController _controllerToken = TextEditingController();
-  bool _enabled = false;
-  bool _autoregister = false;
-  bool _autoLogin = false;
 
   void _resetSettings() {
     setState(() {
@@ -35,27 +27,12 @@ class _OauthViewState extends State<OauthView> {
   bool _dummy = false;
 
   Future<Oauth?> _getData(bool reset) async {
-    // if the users has not changed, no call to the backend
-    if (_settings != null) return _settings;
-
     _settings = await DI.settings?.api().oauth();
-
-    _controllerId.text = _settings?.clientId ?? "";
-    _controllerSecret.text = _settings?.clientSecret ?? "";
-    _controllerAuth.text = _settings?.url ?? "";
-    _controllerToken.text = _settings?.tokenurl ?? "";
-
-    _enabled = _settings?.enabled ?? false;
-    _autoregister = _settings?.autoRegister ?? false;
-    _autoLogin = _settings?.autoLogin ?? false;
-
     return _settings;
   }
 
   @override
   Widget build(BuildContext context) {
-    var theme = Theme.of(context).colorScheme;
-
     return FutureBuilder(
         future: _getData(_dummy),
         builder: (context, snapshot) {
@@ -72,100 +49,141 @@ class _OauthViewState extends State<OauthView> {
 
               // if we got our data
             } else if (snapshot.hasData) {
-              return Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Oauth",
-                        style: Theme.of(context).textTheme.headlineMedium),
-                    const SizedBox(height: 5),
-                    Row(
-                      children: [
-                        const Text("Enabled"),
-                        Switch(
-                            value: _enabled,
-                            onChanged: (bool? value) {
-                              setState(() {
-                                _enabled = value!;
-                              });
-                            })
-                      ],
-                    ),
-                    const SizedBox(height: 5),
-                    SquareTextField(
-                      controller: _controllerId,
-                      label: "Client id",
-                      icon: Icons.person_sharp,
-                      theme: theme,
-                    ),
-                    const SizedBox(height: 10),
-                    SquareTextField(
-                      theme: theme,
-                      controller: _controllerSecret,
-                      label: "Client secret",
-                      icon: Icons.password_sharp,
-                    ),
-                    const SizedBox(height: 10),
-                    SquareTextField(
-                      controller: _controllerAuth,
-                      label: "Auth url",
-                      icon: Icons.connect_without_contact_sharp,
-                      theme: theme,
-                    ),
-                    const SizedBox(height: 10),
-                    SquareTextField(
-                      controller: _controllerToken,
-                      label: "Token url",
-                      icon: Icons.token_sharp,
-                      theme: theme,
-                    ),
-                    const SizedBox(height: 5),
-                    Row(
-                      children: [
-                        const Text("Auto register"),
-                        Switch(
-                            value: _autoregister,
-                            onChanged: (bool? value) {
-                              setState(() {
-                                _autoregister = value!;
-                              });
-                            })
-                      ],
-                    ),
-                    const SizedBox(height: 5),
-                    Row(
-                      children: [
-                        const Text("Auto login"),
-                        Switch(
-                            value: _autoLogin,
-                            onChanged: (bool? value) {
-                              setState(() {
-                                _autoLogin = value!;
-                              });
-                            })
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    SizedBox(
-                      width: 200,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size.fromHeight(50),
-                          shape: const ContinuousRectangleBorder(),
-                        ),
-                        onPressed: submit,
-                        child: const Text("Save"),
-                      ),
-                    ),
-                  ],
-                ),
-              );
+              return OauthFormView(snapshot.data, callback: _resetSettings);
             }
           }
-          return const Center(
-              child: SizedBox(width: 50, height: 50, child: HelseLoader()));
+          return const Center(child: SizedBox(width: 50, height: 50, child: HelseLoader()));
         });
+  }
+}
+
+class OauthFormView extends StatefulWidget {
+  final Oauth? data;
+  final void Function() callback;
+
+  const OauthFormView(this.data, {super.key, required this.callback});
+
+  @override
+  State<OauthFormView> createState() => _OauthFormViewState();
+}
+
+class _OauthFormViewState extends State<OauthFormView> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
+  final TextEditingController _controllerId = TextEditingController();
+  final TextEditingController _controllerSecret = TextEditingController();
+  final TextEditingController _controllerAuth = TextEditingController();
+  final TextEditingController _controllerToken = TextEditingController();
+  bool _enabled = false;
+  bool _autoregister = false;
+  bool _autoLogin = false;
+
+  @override
+  void initState() {
+    super.initState();
+    var data = widget.data;
+    _controllerId.text = data?.clientId ?? "";
+    _controllerSecret.text = data?.clientSecret ?? "";
+    _controllerAuth.text = data?.url ?? "";
+    _controllerToken.text = data?.tokenurl ?? "";
+
+    _enabled = data?.enabled ?? false;
+    _autoregister = data?.autoRegister ?? false;
+    _autoLogin = data?.autoLogin ?? false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context).colorScheme;
+
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Oauth", style: Theme.of(context).textTheme.headlineMedium),
+          const SizedBox(height: 5),
+          Row(
+            children: [
+              const Text("Enabled"),
+              CustomSwitch(
+                  value: _enabled,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      _enabled = value!;
+                    });
+                  })
+            ],
+          ),
+          const SizedBox(height: 5),
+          SquareTextField(
+            controller: _controllerId,
+            label: "Client id",
+            icon: Icons.person_sharp,
+            theme: theme,
+          ),
+          const SizedBox(height: 10),
+          SquareTextField(
+            theme: theme,
+            controller: _controllerSecret,
+            label: "Client secret",
+            icon: Icons.password_sharp,
+          ),
+          const SizedBox(height: 10),
+          SquareTextField(
+            controller: _controllerAuth,
+            label: "Auth url",
+            icon: Icons.connect_without_contact_sharp,
+            theme: theme,
+          ),
+          const SizedBox(height: 10),
+          SquareTextField(
+            controller: _controllerToken,
+            label: "Token url",
+            icon: Icons.token_sharp,
+            theme: theme,
+          ),
+          const SizedBox(height: 5),
+          Row(
+            children: [
+              const Text("Auto register"),
+              CustomSwitch(
+                  value: _autoregister,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      _autoregister = value!;
+                    });
+                  })
+            ],
+          ),
+          const SizedBox(height: 5),
+          Row(
+            children: [
+              const Text("Auto login"),
+              CustomSwitch(
+                  value: _autoLogin,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      _autoLogin = value!;
+                    });
+                  })
+            ],
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            width: 200,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(50),
+                shape: const ContinuousRectangleBorder(),
+              ),
+              onPressed: submit,
+              child: const Text("Save"),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   void submit() async {
@@ -186,7 +204,7 @@ class _OauthViewState extends State<OauthView> {
 
         Notify.show("Saved Successfully");
 
-        _resetSettings();
+        widget.callback();
       }
     } catch (ex) {
       Notify.show("Error: $ex");
