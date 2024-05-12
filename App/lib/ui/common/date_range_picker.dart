41c1952aab40a9cc20acd90838410efc463296e6
FSchiltz <1764553+FSchiltz@users.noreply.github.com>
2024-05-12 11:43:10 +0200

    Add first options to the preset date range

--- App/lib/ui/common/date_range_picker.dart
+++ App/lib/ui/common/date_range_picker.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:helse/helpers/date.dart';
 import 'package:helse/ui/common/date_range_input.dart';
+
+enum DatePreset {
+  today,
+  week,
+  month,
+  trimestre,
+  halfYear,
+  year,
+  yearToDate,
+}
 
 class DateRangePicker extends StatelessWidget {
   final void Function(DateTimeRange value) setDate;
   final DateTimeRange initial;
 
-  const DateRangePicker(
+  DateRangePicker(
     this.setDate,
     this.initial, {
     super.key,
@@ -19,6 +30,24 @@ class DateRangePicker extends StatelessWidget {
         SizedBox(
           width: 50,
           child: IconButton(onPressed: _previousPeriod, icon: const Icon(Icons.skip_previous_sharp)),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: MenuAnchor(
+            menuChildren: [
+              MenuItemButton(onPressed: () => _setPreset(DatePreset.today), child: Text("Today")),
+            ],
+            builder: (context, controller, child) => InkWell(
+              child: const Icon(Icons.calendar_month_sharp),
+              onTap: () {
+                if (controller.isOpen) {
+                  controller.close();
+                } else {
+                  controller.open();
+                }
+              },
+            ),
+          ),
         ),
         Flexible(child: DateRangeInput(_callBack, initial)),
         SizedBox(
@@ -49,5 +78,17 @@ class DateRangePicker extends StatelessWidget {
 
   void _callBack(DateTimeRange date) {
     setDate(date);
+  }
+
+  void _setPreset(DatePreset? value) {
+    if (value == null) return;
+
+    switch (value) {
+      case DatePreset.today:
+        setDate(DateHelper.now());
+        break;
+      default:
+        break;
+    }
   }
 }
