f6ae1eb1b4923f8f413a4f674fca40321b8ffd53
FSchiltz <1764553+FSchiltz@users.noreply.github.com>
2024-06-04 21:21:38 +0200

    Fix date input for patients

--- App/lib/ui/blocs/care/patients_dashboard.dart
+++ App/lib/ui/blocs/care/patients_dashboard.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../../../helpers/date.dart';
 import '../../../services/swagger/generated_code/swagger.swagger.dart';
 import '../../common/date_range_input.dart';
+import '../../common/date_range_picker.dart';
 import '../../patient_dashboard.dart';
 
 class PatientsDashboard extends StatefulWidget {
@@ -25,6 +26,7 @@ class _PatientDashboardState extends State<PatientsDashboard> {
 
   @override
   Widget build(BuildContext context) {
+    var isLargeScreen = MediaQuery.of(context).size.width > 600;
     return Scaffold(
       appBar: AppBar(
         title: Padding(
@@ -34,7 +36,7 @@ class _PatientDashboardState extends State<PatientsDashboard> {
         actions: <Widget>[
           Padding(
             padding: const EdgeInsets.only(right: 20.0),
-            child: DateRangeInput(_setDate, date, true),
+            child: DateRangePicker(_setDate, date, isLargeScreen),
           ),
         ],
       ),
