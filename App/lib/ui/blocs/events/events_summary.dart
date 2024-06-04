3567327d1bc6bf4d92662229a955c5083666908a
Francois Schiltz <1764553+FSchiltz@users.noreply.github.com>
2024-06-04 20:51:56 +0200

    Bugfix/eventspreview (#155)
    * limit the height of the preview
    
    * use dot for empty period

--- App/lib/ui/blocs/events/events_summary.dart
+++ App/lib/ui/blocs/events/events_summary.dart
@@ -1,5 +1,5 @@
 import 'dart:math';
-import 'package:fl_chart/fl_chart.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 
 import '../../../services/swagger/generated_code/swagger.swagger.dart';
@@ -76,6 +76,9 @@ class _EventTimelineState extends State<EventTimeline> {
     List<Widget> chartBars = [];
 
     int tick = 0;
+    int max = data.map((x) => x.data?.values.map((y) => y as int).sum ?? 0).max;
+    var coeff = min(160 / max, 30.0);
+
     for (var d in data) {
       var p = d.data;
       if (p != null) {
@@ -84,12 +87,11 @@ class _EventTimelineState extends State<EventTimeline> {
           child: Container(
             clipBehavior: Clip.antiAlias,
             decoration: BoxDecoration(
-              color: colorScheme.primary,
-              
+              color: null,
               borderRadius: BorderRadius.circular(12),
             ),
             child: Column(
-              children: map(p, tick, colorScheme.primary),
+              children: map(p, tick, colorScheme.primary, coeff: coeff),
             ),
           ),
         ));
@@ -110,7 +112,7 @@ class _EventTimelineState extends State<EventTimeline> {
     );
   }
 
-  List<Widget> map(Map<String, dynamic> p, int tick, Color empty) {
+  List<Widget> map(Map<String, dynamic> p, int tick, Color empty, {required double coeff}) {
     List<Widget> widgets = [];
 
     for (var entry in p.entries) {
@@ -123,17 +125,25 @@ class _EventTimelineState extends State<EventTimeline> {
               color: _stateColor(entry.key),
             ),
             width: 20,
-            height: 30 * count as double,
+            height: coeff * count,
           ),
         ));
       }
     }
 
     if (widgets.isEmpty) {
-      widgets.add(Container(
-        color: empty,
+      widgets.add(SizedBox(
         width: 28,
-        height: 1,
+        child: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              color: empty,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            width: 4,
+            height: 4,
+          ),
+        ),
       ));
     }
 
