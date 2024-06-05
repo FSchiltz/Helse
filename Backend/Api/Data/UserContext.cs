f937a54d70ca00727e0762d21e1b8c4e1844a725
Francois Schiltz <1764553+FSchiltz@users.noreply.github.com>
2024-06-05 11:29:36 +0200

    Fix the caregiver sharing (#159)

--- Backend/Api/Data/UserContext.cs
+++ Backend/Api/Data/UserContext.cs
@@ -19,7 +19,7 @@ public interface IUserContext : IContext
     Task<long> InsertPerson(PersonCreation newUser);
     Task InsertUser(PersonCreation newUser, long id, string password);
     Task UpdatePassword(long user, string password);
-    Task<List<PersonFromDb>> GetUsers(int? role);
+    Task<List<PersonFromDb>> GetUsers();
     Task<List<Models.Right>> GetRights(DateTime time);
     Task SetExpiryRight(long personId, DateTime now);
     Task InsertRights(IEnumerable<Models.Right> dbRights);
@@ -110,15 +110,12 @@ public class UserContext(DataConnection db) : IUserContext
         });
     }
 
-    public Task<List<PersonFromDb>> GetUsers(int? role)
+    public Task<List<PersonFromDb>> GetUsers()
     {
         var query =
         from u in db.GetTable<User>()
         from p in db.GetTable<Models.Person>().RightJoin(pr => pr.Id == u.PersonId)
         select new PersonFromDb(u, p);
-
-        if (role != null)
-            query = query.Where(x => x.User.Type == role);
 
         return query.OrderBy(x => x.User.Id).ToListAsync();
     }
