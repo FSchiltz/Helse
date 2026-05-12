e02f0f4d945b0717484a847451f2a6e19b36c313
Francois Schiltz <1764553+FSchiltz@users.noreply.github.com>
2026-05-12 20:17:13 +0200

    split the settings

---
+++ flutter_frontend/lib/ui/blocs/localSettings/event_settings.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:helse/logic/d_i.dart';
+import 'package:helse/logic/settings/events_settings.dart';
+import 'package:helse/logic/settings/ordered_item.dart';
+import 'package:helse/logic/settings/settings_logic.dart';
+import 'package:helse/ui/common/loader.dart';
+import 'package:helse/ui/common/notification.dart';
+import 'package:helse/ui/common/ordered_list.dart';
+
+class EventSettings extends StatefulWidget {
+  const EventSettings({super.key});
+
+  @override
+  State<EventSettings> createState() => _EventSettingsState();
+}
+
+class _EventSettingsState extends State<EventSettings> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
+  List<OrderedItem> _events = [];
+
+  Future<int> _getData() async {
+    _events = (await SettingsLogic.getEvents()).events;
+
+    return 1;
+  }
+
+  void _submitEvent() async {
+    try {
+      if (_formKey.currentState?.validate() ?? false) {
+        // save the user's settings
+        await DI.settings.saveEvents(EventsSettings(_events));
+
+        Notify.show("Saved Successfully");
+        _getData();
+      }
+    } catch (ex) {
+      Notify.showError("Error: $ex");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _getData(),
+      builder: (context, snapshot) {
+        // Checking if future is resolved
+        if (snapshot.connectionState == ConnectionState.done) {
+          // If we got an error
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                '${snapshot.error} occurred',
+                style: const TextStyle(fontSize: 18),
+              ),
+            );
+            // if we got our data
+          } else if (snapshot.hasData) {
+            return Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "Events",
+                        style: Theme.of(context).textTheme.headlineSmall,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SizedBox(
+                          width: 80,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: const Size.fromHeight(40),
+                              shape: const ContinuousRectangleBorder(),
+                            ),
+                            onPressed: _submitEvent,
+                            child: const Text("Save"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  OrderedList(_events),
+                ],
+              ),
+            );
+          }
+        }
+        return const Center(
+          child: SizedBox(width: 50, height: 50, child: HelseLoader()),
+        );
+      },
+    );
+  }
+}
