be940ffb39e6cee7e936e37b5e5e34c5a941f924
Francois Schiltz <1764553+FSchiltz@users.noreply.github.com>
2026-05-26 11:43:54 +0200

    use pagination for the detail

--- flutter_frontend/lib/ui/blocs/metrics/metric_graph.dart
+++ flutter_frontend/lib/ui/blocs/metrics/metric_graph.dart
@@ -104,78 +104,31 @@ class _MetricGraphState extends State<MetricGraph> {
           ),
         ),
         Flexible(
-          child: SingleChildScrollView(
-            child: DataTable(
-              columns: const [
-                DataColumn(label: Expanded(child: Text("Id"))),
-                DataColumn(label: Expanded(child: Text("Value"))),
-                DataColumn(label: Expanded(child: Text("Date"))),
-                DataColumn(label: Expanded(child: Text("Type"))),
-                DataColumn(label: Expanded(child: Text("Source"))),
-                DataColumn(label: Expanded(child: Text(""))),
-              ],
-
-              rows:
-                  metric?.metrics
-                      .map(
-                        (m) => DataRow(
-                          cells: [
-                            DataCell(Text((m.id).toString())),
-                            DataCell(Text('${m.value}${widget.type.unit}')),
-                            DataCell(
-                              Text(
-                                DateHelper.format(
-                                  m.date.toLocal(),
-                                  context: context,
-                                ),
-                              ),
-                            ),
-                            DataCell(Text(m.tag.toString())),
-                            DataCell(Text(m.source.toString())),
-                            DataCell(
-                              Row(
-                                children: [
-                                  IconButton(
-                                    onPressed: () {
-                                      showDialog<void>(
-                                        context: context,
-                                        builder: (BuildContext context) {
-                                          return MetricAdd(
-                                            widget.type,
-                                            widget.reset,
-                                            person: widget.person,
-                                            edit: m,
-                                          );
-                                        },
-                                      );
-                                    },
-                                    icon: const Icon(Icons.edit_sharp),
-                                  ),
-                                  IconButton(
-                                    onPressed: () {
-                                      showDialog<void>(
-                                        context: context,
-                                        builder: (BuildContext context) {
-                                          return DeleteMetric(() async {
-                                            await DI.metric.deleteMetrics(m.id);
-                                            widget.reset();
-                                            setState(() {
-                                              _metric = null;
-                                            });
-                                          }, person: widget.person);
-                                        },
-                                      );
-                                    },
-                                    icon: const Icon(Icons.delete_sharp),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
-                      .toList() ??
-                  [],
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SingleChildScrollView(
+              child: Center(
+                child: PaginatedDataTable(
+                  rowsPerPage: 50,
+                  showEmptyRows: false,                  
+                  primary: true,
+                  columns: const [
+                    DataColumn(label: Expanded(child: Text("Id"))),
+                    DataColumn(label: Expanded(child: Text("Value"))),
+                    DataColumn(label: Expanded(child: Text("Date"))),
+                    DataColumn(label: Expanded(child: Text("Tag"))),
+                    DataColumn(label: Expanded(child: Text("Source"))),
+                    DataColumn(label: Expanded(child: Text(""))),
+                  ],
+                  source: MetricDataSource(
+                    metric?.metrics ?? [],
+                    context,
+                    widget.person,
+                    widget.type,
+                    reset: widget.reset,
+                  ),
+                ),
+              ),
             ),
           ),
         ),
@@ -357,4 +310,74 @@ class _MetricGraphState extends State<MetricGraph> {
 
     return groups;
   }
+}
+
+class MetricDataSource extends DataTableSource {
+  final List<Metric> metrics;
+  final BuildContext context;
+  final int? person;
+  final MetricType type;
+  final void Function() reset;
+
+  MetricDataSource(
+    this.metrics,
+    this.context,
+    this.person,
+    this.type, {
+    required this.reset,
+  });
+
+  @override
+  DataRow? getRow(int index) {
+    var m = metrics[index];
+    return DataRow(
+      cells: [
+        DataCell(Text((m.id).toString())),
+        DataCell(Text('${m.value} ${type.unit}')),
+        DataCell(Text(DateHelper.format(m.date.toLocal(), context: context))),
+        DataCell(Text(m.tag.toString())),
+        DataCell(Text(m.source.toString())),
+        DataCell(
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {
+                  showDialog<void>(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return MetricAdd(type, reset, person: person, edit: m);
+                    },
+                  );
+                },
+                icon: const Icon(Icons.edit_sharp),
+              ),
+              IconButton(
+                onPressed: () {
+                  showDialog<void>(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return DeleteMetric(() async {
+                        await DI.metric.deleteMetrics(m.id);
+                        reset();
+                      }, person: person);
+                    },
+                  );
+                },
+                icon: const Icon(Icons.delete_sharp),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get rowCount => metrics.length;
+
+  @override
+  int get selectedRowCount => 0;
 }
