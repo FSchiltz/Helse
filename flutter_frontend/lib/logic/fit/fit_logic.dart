5229f97833d8b874d9f1c4554c8edbb5e63e81d0
Francois Schiltz <1764553+FSchiltz@users.noreply.github.com>
2026-05-30 11:22:39 +0200

    add sleep

--- flutter_frontend/lib/logic/fit/fit_logic.dart
+++ flutter_frontend/lib/logic/fit/fit_logic.dart
@@ -60,27 +60,27 @@ class FitLogic {
     HealthDataType.HEIGHT,
     HealthDataType.BODY_TEMPERATURE,
     HealthDataType.SKIN_TEMPERATURE,
+    HealthDataType.SLEEP_ASLEEP,
+    HealthDataType.SLEEP_AWAKE,
+    HealthDataType.SLEEP_DEEP,
+    HealthDataType.SLEEP_LIGHT,
+    HealthDataType.SLEEP_REM,
+    HealthDataType.SLEEP_SESSION,
+    HealthDataType.SLEEP_UNKNOWN,
   ];
 
   final List<HealthDataType> _other = [
-    HealthDataType.SLEEP_WRIST_TEMPERATURE,
+    HealthDataType.TOTAL_CALORIES_BURNED,
     HealthDataType.RESTING_HEART_RATE,
     HealthDataType.WALKING_HEART_RATE,
+    HealthDataType.SLEEP_WRIST_TEMPERATURE,
     HealthDataType.BASAL_ENERGY_BURNED,
     HealthDataType.DISTANCE_WALKING_RUNNING,
     HealthDataType.DISTANCE_SWIMMING,
     HealthDataType.DISTANCE_CYCLING,
-    HealthDataType.SLEEP_ASLEEP,
-    HealthDataType.SLEEP_AWAKE_IN_BED,
-    HealthDataType.SLEEP_AWAKE,
-    HealthDataType.SLEEP_DEEP,
     HealthDataType.SLEEP_IN_BED,
-    HealthDataType.SLEEP_LIGHT,
     HealthDataType.SLEEP_OUT_OF_BED,
-    HealthDataType.SLEEP_REM,
-    HealthDataType.SLEEP_SESSION,
-    HealthDataType.SLEEP_UNKNOWN,
-    HealthDataType.TOTAL_CALORIES_BURNED,
+    HealthDataType.SLEEP_AWAKE_IN_BED,
   ];
 
   Future<void> requestPermissions() async {
