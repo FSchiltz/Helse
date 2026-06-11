bf0fda05d47f59b8989c231c2f788e95bf2d2b64
Francois Schiltz <1764553+FSchiltz@users.noreply.github.com>
2026-06-11 09:54:25 +0200

    use icons for the widget

--- flutter_frontend/lib/ui/blocs/metrics/widget/metric_widget.dart
+++ flutter_frontend/lib/ui/blocs/metrics/widget/metric_widget.dart
@@ -1,7 +1,6 @@
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:helse/di/dependencies.dart';
-import 'package:helse/helpers/translation.dart';
 import 'package:helse/ui/blocs/metrics/detail/metric_detail_page.dart';
 import 'package:helse/ui/common/loading_builder.dart';
 
@@ -105,14 +104,11 @@ class _MetricWidgetState extends State<MetricWidget> {
                 if (data.isNotEmpty)
                   Expanded(
                     child: Align(
-                      alignment: AlignmentGeometry.topLeft,
-                      child: Text(
-                        _getTextInfo(data, widget.type, context),
-                        style: Theme.of(context).textTheme.bodyMedium,
-                        textAlign: TextAlign.start,
-                      ),
+                      alignment: AlignmentGeometry.topCenter,
+                      child: _getTextInfo(data, widget.type),
                     ),
                   ),
+
                 Flexible(
                   child: MetricCondensed(
                     data,
@@ -130,21 +126,18 @@ class _MetricWidgetState extends State<MetricWidget> {
     );
   }
 
-  String _getTextInfo(
-    List<Metric> metrics,
-    MetricType type,
-    BuildContext context,
-  ) {
-    var locale = Translation.of(context);
+  Widget _getTextInfo(List<Metric> metrics, MetricType type) {
     String? value;
+    Icon? icon;
     switch (type.summaryType) {
       case MetricSummary.sum:
-        value =
-            '${locale.total} ${metrics.map((metric) => double.parse(metric.value)).sum}';
+        icon = Icon(Icons.data_exploration_outlined);
+        value = '${metrics.map((metric) => double.parse(metric.value)).sum}';
         break;
       case MetricSummary.mean:
+        icon = Icon(Icons.update);
         value =
-            '${locale.mean} ${(metrics.map((metric) => double.parse(metric.value)).sum / metrics.length).round()}';
+            '${(metrics.map((metric) => double.parse(metric.value)).sum / metrics.length).round()}';
         break;
       case MetricSummary.latest:
       default:
@@ -156,6 +149,19 @@ class _MetricWidgetState extends State<MetricWidget> {
       value += ' ${type.unit.code}';
     }
 
-    return value;
+    var text = Text(
+      value,
+      style: Theme.of(context).textTheme.bodyMedium,
+      textAlign: TextAlign.start,
+    );
+
+    if (icon == null) {
+      return text;
+    }
+
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [icon, SizedBox(width: 2), text],
+    );
   }
 }
