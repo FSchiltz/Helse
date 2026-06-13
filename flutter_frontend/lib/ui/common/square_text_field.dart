3cd02e81121acb352505f70093b985807d952046
Francois Schiltz <1764553+FSchiltz@users.noreply.github.com>
2026-06-13 21:56:22 +0200

    first new version of a timepicker

--- flutter_frontend/lib/ui/common/square_text_field.dart
+++ flutter_frontend/lib/ui/common/square_text_field.dart
@@ -15,8 +15,10 @@ class SquareTextField extends StatelessWidget {
     this.onIconPressed,
     this.type,
     this.suffixIcon,
+    this.onTap,
   });
 
+  final void Function()? onTap;
   final IconButton? suffixIcon;
   final void Function()? onEditingComplete;
   final String label;
