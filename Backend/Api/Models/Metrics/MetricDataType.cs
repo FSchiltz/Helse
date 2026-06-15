2504eddc3e5cf8dc8ce1fc13ac7f033c1dcf3853
Francois Schiltz <1764553+FSchiltz@users.noreply.github.com>
2026-06-15 14:30:53 +0200

    Add a min max type of metric

--- Backend/Api/Models/Metrics/MetricDataType.cs
+++ Backend/Api/Models/Metrics/MetricDataType.cs
@@ -2,7 +2,14 @@ namespace Api.Models.Metrics;
 
 public enum MetricDataType
 {
-    Text,
-    Number,
-    Bool,
+    Text = 0,
+    Number = 1,
+    /// <summary>
+    /// True or false
+    /// </summary>
+    Bool = 2,
+    /// <summary>
+    /// A min and max value (2 numbers separated by a ';')
+    /// </summary>
+    MinMax = 3
 }
