63231654b877bacac36c4f0409d93b768dada5e4
Francois Schiltz <1764553+FSchiltz@users.noreply.github.com>
2026-06-17 22:02:33 +0200

    add options to choose the type to sync

--- flutter_frontend/lib/logic/settings/health_settings.dart
+++ flutter_frontend/lib/logic/settings/health_settings.dart
@@ -2,21 +2,48 @@ class HealthSettings {
   static const _syncHealth = "syncHealth";
   static const _background = "background";
   static const _history = "history";
+  static const _records = "records";
   bool syncHealth;
   bool background;
   bool history;
+  Map<String, HealthRecordSettings> records;
 
-  HealthSettings(this.syncHealth, this.history, this.background);
+  HealthSettings(this.syncHealth, this.history, this.background, this.records);
 
   // stupid boilerplate code because dart can't decode json
-  HealthSettings.fromJson(dynamic json)
-    : syncHealth = json[_syncHealth] as bool? ?? false,
-      history = json[_history] as bool? ?? false,
-      background = json[_background] as bool? ?? false;
+  factory HealthSettings.fromJson(Map<String, dynamic> json) {
+    final recordsJson = json[_records] as Map<String, dynamic>;
+
+    return HealthSettings(
+      json[_syncHealth] as bool? ?? false,
+      json[_history] as bool? ?? false,
+      json[_background] as bool? ?? false,
+      recordsJson.map(
+        (key, value) => MapEntry(
+          key,
+          HealthRecordSettings.fromJson(value as Map<String, dynamic>),
+        ),
+      ),
+    );
+  }
 
   Map<String, dynamic> toJson() => {
     _syncHealth: syncHealth,
     _background: background,
     _history: history,
+    _records: records.map((key, value) => MapEntry(key, value.toJson())),
   };
+}
+
+class HealthRecordSettings {
+  static const String _sync = "sync";
+  bool sync;
+
+  HealthRecordSettings(this.sync);
+
+  factory HealthRecordSettings.fromJson(Map<String, dynamic> json) {
+    return HealthRecordSettings(json[_sync] as bool? ?? false);
+  }
+
+  Map<String, dynamic> toJson() => {_sync: sync};
 }
