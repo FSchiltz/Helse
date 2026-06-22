fbe13c4ae4742ea471454dfcd3f66fe13a41f9d3
Francois Schiltz <1764553+FSchiltz@users.noreply.github.com>
2026-06-22 19:44:31 +0200

    fix off by one

--- flutter_frontend/lib/helpers/string_helper.dart
+++ flutter_frontend/lib/helpers/string_helper.dart
@@ -2,6 +2,6 @@ extension Case on Iterable<String> {
   String toCamel() {
     final firstChar = first[0];
     final rest = map((e) => e.toLowerCase()).join(' ');
-    return '${firstChar.toUpperCase()}${rest.substring(1, rest.length - 1)}';
+    return '${firstChar.toUpperCase()}${rest.substring(1, rest.length)}';
   }
 }
