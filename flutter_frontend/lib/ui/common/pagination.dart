a6037d1b8065a8892cf39eb7bcfd22c4d4813452
Francois Schiltz <1764553+FSchiltz@users.noreply.github.com>
2026-06-24 17:25:29 +0200

    fix the pagination when one page

--- flutter_frontend/lib/ui/common/pagination.dart
+++ flutter_frontend/lib/ui/common/pagination.dart
@@ -22,25 +22,28 @@ class Pagination extends StatelessWidget {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
-        IconButton(
-          onPressed: page <= 0 ? null : () => callBack(max(0, page - 1)),
-          icon: Icon(Icons.arrow_left),
-          iconSize: 40,
-          padding: EdgeInsets.all(2),
-        ),
-        Text('${page + 1} / ${maxPage + 1} pages'),
-        SizedBox(width: UIConstants.formPad),
-        Text('-'),
-        SizedBox(width: UIConstants.formPad),
+        if (maxPage > 1) ...[
+          IconButton(
+            onPressed: page <= 0 ? null : () => callBack(max(0, page - 1)),
+            icon: Icon(Icons.arrow_left),
+            iconSize: 40,
+            padding: EdgeInsets.all(2),
+          ),
+          Text('${page + 1} / ${maxPage + 1} pages'),
+          SizedBox(width: UIConstants.formPad),
+          Text('-'),
+          SizedBox(width: UIConstants.formPad),
+        ],
         Text('$count items'),
-        IconButton(
-          onPressed: page >= maxPage
-              ? null
-              : () => callBack(min(maxPage, page + 1)),
-          icon: Icon(Icons.arrow_right),
-          iconSize: 40,
-          padding: EdgeInsets.all(2),
-        ),
+        if (maxPage > 1)
+          IconButton(
+            onPressed: page >= maxPage
+                ? null
+                : () => callBack(min(maxPage, page + 1)),
+            icon: Icon(Icons.arrow_right),
+            iconSize: 40,
+            padding: EdgeInsets.all(2),
+          ),
       ],
     );
   }
