bf2f240aa6c71e860b2bf81d9ba4e83cd2fa9314
Francois Schiltz <1764553+FSchiltz@users.noreply.github.com>
2026-06-29 10:37:45 +0200

    add notification first test

--- flutter_frontend/lib/ui/common/notification.dart
+++ flutter_frontend/lib/ui/common/notification.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:helse/main.dart';
 
 enum NotificationKind { info, warning, error }
 
 class Notify {
+  static FlutterLocalNotificationsPlugin? _flutterLocalNotificationsPlugin;
+
   static void simple(String content, NotificationKind kind) {
     final snackBar = SnackBar(
       content: Text(content),
@@ -40,5 +43,44 @@ class Notify {
     // Find the ScaffoldMessenger in the widget tree
     // and use it to show a SnackBar.
     ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  static Future<void> init() async {
+    _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    final initializationSettings = InitializationSettings(
+      android: AndroidInitializationSettings("app_icon"),
+      linux: LinuxInitializationSettings(defaultActionName: "open"),
+    );
+    await _flutterLocalNotificationsPlugin?.initialize(
+      settings: initializationSettings,
+    );
+  }
+
+  static Future<void> showBackground(
+    String content,
+    NotificationKind kind,
+  ) async {
+    const AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails(
+          'your channel id',
+          'your channel name',
+          channelDescription: 'your channel description',
+          importance: Importance.max,
+          priority: Priority.high,
+          ticker: 'ticker',
+        );
+    const NotificationDetails notificationDetails = NotificationDetails(
+      android: androidNotificationDetails,
+      linux: LinuxNotificationDetails(),
+      web: WebNotificationDetails(),
+    );
+    
+    await _flutterLocalNotificationsPlugin?.show(
+      id: 0,
+      title: 'plain title',
+      body: 'plain body',
+      notificationDetails: notificationDetails,
+      payload: 'item x',
+    );
   }
 }
