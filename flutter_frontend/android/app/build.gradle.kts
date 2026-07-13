31d957a905854a2901b9e1e40431ae02e07633af
Francois Schiltz <1764553+FSchiltz@users.noreply.github.com>
2026-07-13 16:20:33 +0200

    use kts for android

--- flutter_frontend/android/app/build.gradle.kts
+++ flutter_frontend/android/app/build.gradle.kts
@@ -1,4 +1,4 @@
-plugins {
+plugins{
     id "com.android.application"
     id "kotlin-android"
     id "dev.flutter.flutter-gradle-plugin"
@@ -29,25 +29,25 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace "com.fschiltz.helse"
-    compileSdk flutter.compileSdkVersion
+    namespace("com.fschiltz.helse")
+    compileSdk(flutter.compileSdkVersion)
     ndkVersion = "28.2.13676358"
 
     compileOptions {
-        coreLibraryDesugaringEnabled true
-        sourceCompatibility JavaVersion.VERSION_17
-        targetCompatibility JavaVersion.VERSION_17
+        coreLibraryDesugaringEnabled =true
+        sourceCompatibility= JavaVersion.VERSION_17
+        targetCompatibility =JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
+    kotlinOptions{
         jvmTarget = '17'
     }
 
-    sourceSets {
+    sourceSets{
         main.java.srcDirs += 'src/main/kotlin'
     }
 
-    defaultConfig {
+    defaultConfig{
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId "com.fschiltz.helse"
         // You can update the following values to match your application needs.
@@ -59,28 +59,38 @@ android {
     }
 
 
-    signingConfigs {
-        release {
-            keyAlias keystoreProperties['keyAlias']
-            keyPassword keystoreProperties['keyPassword']
-            storeFile keystoreProperties['storeFile'] ? file(keystoreProperties['storeFile']) : null
-            storePassword keystoreProperties['storePassword']
+    signingConfigs{
+         if (System.getenv("CI") == "true") {
+            create("release") {
+                keyAlias = System.getenv("KEY_ALIAS")
+                keyPassword = System.getenv("KEY_PASSWORD")
+                storeFile = System.getenv("KEYSTORE_PATH")?.let { file(it) }
+                storePassword = System.getenv("KEYSTORE_PASSWORD")
+            }
         }
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig signingConfigs.release
+       release {
+            signingConfig = (System.getenv("CI") == "true")? signingConfigs.getByName("release"): signingConfigs.getByName("debug")
+            
+
+            // Enable code shrinking to reduce APK size
+            isMinifyEnabled = true
+            isShrinkResources = true
         }
+    }
+
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
     }
 }
 
 flutter {
-    source '../..'
+    source('../..')
 }
 
 dependencies {
-    coreLibraryDesugaring 'com.android.tools:desugar_jdk_libs:2.1.4'
+    coreLibraryDesugaring('com.android.tools:desugar_jdk_libs:2.1.4')
 }
