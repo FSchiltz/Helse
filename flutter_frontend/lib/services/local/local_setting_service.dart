6415db6334457b17d4f98ded6bae9702139b350a
Francois Schiltz <1764553+FSchiltz@users.noreply.github.com>
2026-08-03 21:16:49 +0200

    add the settings code

--- flutter_frontend/lib/services/local/local_setting_service.dart
+++ flutter_frontend/lib/services/local/local_setting_service.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:helse/di/dependencies.dart';
 import 'package:helse/services/local/local_service.dart';
 import 'package:helse/services/settings_services.dart';
@@ -8,12 +9,67 @@ class LocalSettingService extends LocalService implements SettingService {
 
   @override
   Future<PatientsSettings> getPatientsSettings() async {
-    return Dependencies.logics.patientsSettings.patientsSettings();
+    var settings = Dependencies.logics.patientsSettings.patientsSettings();
+
+    settings = settings.copyWith(
+      $default: settings.$default ?? PatientSettings(),
+      patients: settings.patients ?? [],
+    );
+
+    final metricTypes = await Dependencies.services.metric.metricsType(
+      false,
+      null,
+    );
+    final metricGroups = await Dependencies.services.metric.metricsGroup();
+    final eventTypes = await Dependencies.services.event.eventsType(false);
+
+    final patients = <PatientSettings>[];
+
+    for (final patient in settings.patients!) {
+      patients.add(
+        updatePatient(patient, metricTypes, metricGroups, eventTypes),
+      );
+    }
+
+    return settings.copyWith(
+      $default: updatePatient(
+        settings.$default,
+        metricTypes,
+        metricGroups,
+        eventTypes,
+      ),
+      patients: patients,
+    );
   }
 
   @override
   Future<UserSettings> getPersonSettings() async {
-    return Dependencies.logics.settings.userSettings();
+    var settings = Dependencies.logics.settings.userSettings();
+
+    settings = settings.copyWith(
+      metricSettings:
+          settings.metricSettings ?? MetricSettings(displaySettings: []),
+      groups: settings.groups ?? MetricGroupSettings(displaySettings: []),
+      eventSettings:
+          settings.eventSettings ??
+          EventSettings(displaySettings: [], displayValueSettings: []),
+    );
+
+    final metricTypes = await Dependencies.services.metric.metricsType(
+      false,
+      null,
+    );
+
+    updateMetrics(settings.metricSettings!.displaySettings, metricTypes ?? []);
+
+    final metricGroups = await Dependencies.services.metric.metricsGroup();
+    updateMetricGroups(settings.groups!.displaySettings, metricGroups ?? []);
+
+    // update the events
+    final eventTypes = await Dependencies.services.event.eventsType(false);
+    updateEvents(settings.eventSettings!.displaySettings, eventTypes ?? []);
+
+    return settings;
   }
 
   @override
@@ -45,4 +101,99 @@ class LocalSettingService extends LocalService implements SettingService {
 
   @override
   Future<void> updateSmtp(Smtp settings) async {}
+
+  void updateMetrics(
+    List<OrderedItem> displaySettings,
+    List<MetricType> metricTypes,
+  ) {
+    final data = displaySettings.toList();
+    displaySettings.clear();
+    for (var metric in metricTypes) {
+      var existing = data.firstWhereOrNull(
+        (element) => element.id == metric.id,
+      );
+      if (existing != null) {
+        displaySettings.add(
+          existing.copyWith(name: metric.name, parent: metric.groupId),
+        );
+      } else {
+        displaySettings.add(
+          Dependencies.logics.settings.getDefaultMetricType(metric),
+        );
+      }
+    }
+  }
+
+  void updateMetricGroups(
+    List<OrderedItem> displaySettings,
+    List<Group> metricGroups,
+  ) {
+    final data = displaySettings.toList();
+    displaySettings.clear();
+    for (final group in metricGroups) {
+      final existing = data.firstWhereOrNull(
+        (element) => element.id == group.id,
+      );
+      if (existing != null) {
+        displaySettings.add(existing.copyWith(name: group.name));
+      } else {
+        displaySettings.add(
+          Dependencies.logics.settings.getDefaultGroupType(group),
+        );
+      }
+    }
+  }
+
+  void updateEvents(
+    List<OrderedItem> displaySettings,
+    List<EventType> eventTypes,
+  ) {
+    final data = displaySettings.toList();
+    displaySettings.clear();
+    for (final event in eventTypes) {
+      final existing = data.firstWhereOrNull(
+        (element) => element.id == event.id,
+      );
+      if (existing != null) {
+        displaySettings.add(
+          existing.copyWith(name: event.name, parent: event.groupId),
+        );
+      } else {
+        displaySettings.add(
+          Dependencies.logics.settings.getDefaultEventType(event),
+        );
+      }
+    }
+  }
+
+  PatientSettings updatePatient(
+    PatientSettings? settings,
+    List<MetricType>? metricTypes,
+    List<Group>? metricGroups,
+    List<EventType>? eventTypes,
+  ) {
+    settings =
+        settings?.copyWith(
+          metricSettings:
+              settings.metricSettings ?? MetricSettings(displaySettings: []),
+          groups: settings.groups ?? MetricGroupSettings(displaySettings: []),
+          eventSettings:
+              settings.eventSettings ??
+              EventSettings(displaySettings: [], displayValueSettings: []),
+        ) ??
+        PatientSettings(
+          eventSettings: EventSettings(
+            displaySettings: [],
+            displayValueSettings: [],
+          ),
+          groups: MetricGroupSettings(displaySettings: []),
+          metricSettings: MetricSettings(displaySettings: []),
+        );
+
+    updateMetrics(settings.metricSettings!.displaySettings, metricTypes ?? []);
+    updateMetricGroups(settings.groups!.displaySettings, metricGroups ?? []);
+    updateEvents(settings.eventSettings!.displaySettings, eventTypes ?? []);
+
+    return settings;
+  }
 }
