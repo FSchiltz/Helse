1300c3bc8ffd7a4741a22e169689ebf32140ffa4
Francois Schiltz <1764553+FSchiltz@users.noreply.github.com>
2026-08-13 11:25:39 +0200

    Add b asic support for tasks and index

--- flutter_frontend/lib/services/local/local_import_service.dart
+++ flutter_frontend/lib/services/local/local_import_service.dart
@@ -6,6 +6,8 @@ import 'package:helse/services/local/local_service.dart';
 import 'package:helse/services/swagger/generated_code/helseapi.swagger.dart';
 
 class LocalImportService extends LocalService implements ImportService {
+  static final Map<String, JobResult> _jobs = {};
+
   @override
   Future<List<ImportType>?> fileTypes() async {
     return [];
@@ -13,7 +15,9 @@ class LocalImportService extends LocalService implements ImportService {
 
   @override
   Future<List<JobResultInfo>> getJobs() async {
-    return [];
+    return _jobs.entries
+        .map((e) => JobResultInfo(id: e.key, result: e.value))
+        .toList();
   }
 
   @override
@@ -23,51 +27,99 @@ class LocalImportService extends LocalService implements ImportService {
 
   @override
   Future<ImportsResult?> importData(ImportData file, {int? person}) async {
-    for (final metric in file.metrics ?? <CreateMetric>[]) {
-      final exists =
-          await (database.metric.select()..where(
-                (x) =>
-                    x.person.equals(person ?? 0) &
-                    x.type.equals(metric.type) &
-                    x.sourceId.equals(metric.sourceId) &
-                    x.source.equals(
-                      metric.source?.name ?? ImportTypes.none.name,
-                    ),
-              ))
-              .getSingleOrNull();
+    final id = _jobs.length.toString();
+    _jobs[id] = JobResult(
+      description: "Import",
+      userId: person ?? 0,
+      start: DateTime.now(),
+      enque: DateTime.now(),
+      progress: 0,
+      status: JobStatus.inprogress,
+    );
 
-      if (exists == null) {
-        await Dependencies.services.metric.addMetrics(metric, person: person);
+    final total = (file.metrics?.length ?? 0) + (file.events?.length ?? 0);
+    var count = 0;
+    var skippedMetric = 0;
+    var addedMetric = 0;
+    var skippedEvent = 0;
+    var addedEvent = 0;
+
+    try {
+      for (final metric in file.metrics ?? <CreateMetric>[]) {
+        final exists =
+            await (database.metric.select()..where(
+                  (x) =>
+                      x.person.equals(person ?? 0) &
+                      x.type.equals(metric.type) &
+                      x.sourceId.equals(metric.sourceId) &
+                      x.source.equals(
+                        metric.source?.name ?? ImportTypes.none.name,
+                      ),
+                ))
+                .getSingleOrNull();
+
+        if (exists == null) {
+          await Dependencies.services.metric.addMetrics(metric, person: person);
+          addedMetric++;
+        } else {
+          skippedMetric++;
+        }
+        count++;
+
+        _jobs[id] = _jobs[id]!.copyWith(
+          progress: (count / total.toDouble()) * 100,
+        );
       }
-    }
 
-    for (final event in file.events ?? <CreateEvent>[]) {
-      final exists =
-          await (database.event.select()..where(
-                (x) =>
-                    x.person.equals(person ?? 0) &
-                    x.source.equals(
-                      event.source?.name ?? ImportTypes.none.name,
-                    ) &
-                    x.sourceId.equals(event.sourceId) &
-                    x.type.equals(event.type),
-              ))
-              .getSingleOrNull();
+      for (final event in file.events ?? <CreateEvent>[]) {
+        final exists =
+            await (database.event.select()..where(
+                  (x) =>
+                      x.person.equals(person ?? 0) &
+                      x.source.equals(
+                        event.source?.name ?? ImportTypes.none.name,
+                      ) &
+                      x.sourceId.equals(event.sourceId) &
+                      x.type.equals(event.type),
+                ))
+                .getSingleOrNull();
 
-      if (exists == null) {
-        await Dependencies.services.event.addEvent(event, person: person);
+        if (exists == null) {
+          await Dependencies.services.event.addEvent(event, person: person);
+          addedEvent++;
+        } else {
+          skippedEvent++;
+        }
+
+        count++;
+
+        _jobs[id] = _jobs[id]!.copyWith(
+          progress: (count / total.toDouble()) * 100,
+        );
       }
+
+      _jobs[id] = _jobs[id]!.copyWith(
+        progress: 100,
+        status: JobStatus.done,
+        stop: DateTime.now(),
+      );
+    } catch (e) {
+      _jobs[id] = _jobs[id]!.copyWith(
+        status: JobStatus.inerror,
+        error: e.toString(),
+        stop: DateTime.now(),
+      );
     }
 
     return ImportsResult(
       events: ImportResult(
-        imported: file.events?.length ?? 0,
-        skipped: 0,
+        imported: addedEvent,
+        skipped: skippedEvent,
         failed: 0,
       ),
       metrics: ImportResult(
-        imported: file.metrics?.length ?? 0,
-        skipped: 0,
+        imported: addedMetric,
+        skipped: skippedMetric,
         failed: 0,
       ),
     );
@@ -75,6 +127,6 @@ class LocalImportService extends LocalService implements ImportService {
 
   @override
   Future<JobResult?> status(String id) async {
-    return null;
+    return _jobs[id];
   }
 }
